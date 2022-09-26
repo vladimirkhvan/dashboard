29a60c8daddfa77905bff1a865d0d0f16d6f91ab
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-09-27 00:08:06 +0500

    removed all controllers into controllers folder, added checkauth middleware

--- backend/index.js
+++ backend/index.js
@@ -1,86 +1,33 @@
 import express from 'express';
-import mysql from 'mysql';
 import cors from 'cors';
+import checkAuth from './utils/checkAuth.js';
+
+import {
+    loginUser,
+    registerUser,
+    blockUsers,
+    unblockUsers,
+    deleteUsers,
+    getUsers,
+} from './controllers/UserController.js';
 
 const app = express();
-
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'test',
-});
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json('hello, i am back');
-});
+app.get('/users', checkAuth, getUsers);
 
-app.get('/users', (req, res) => {
-    const q = 'SELECT * FROM users';
-    db.query(q, (err, data) => {
-        if (err) {
-            return res.json(err);
-        }
-        return res.json(data);
-    });
-});
+app.post('/users', registerUser);
 
-function getFullDate() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
-    const day = date.getDate() < 9 ? '0' + date.getDate() : date.getDate();
-    return year + '-' + month + '-' + day;
-}
+app.post('/users/login', loginUser);
 
-app.post('/users', (req, res) => {
-    const q =
-        'INSERT INTO users (`username`, `password`, `email`, `createdAt`, `lastVisit`, `status`) VALUES (?)';
+app.put('/users/block', checkAuth, blockUsers);
 
-    const values = [
-        req.body.username,
-        req.body.password,
-        req.body.email,
-        getFullDate(),
-        getFullDate(),
-        'active',
-    ];
+app.put('/users/unblock', checkAuth, unblockUsers);
 
-    db.query(q, [values], (err, data) => {
-        if (err) {
-            return res.status(409).json(err);
-        }
-
-        return res.json('User was added successfully');
-    });
-});
-
-app.get('/users/:email/:password', (req, res) => {
-    const userEmail = req.params.email;
-    const userPassword = req.params.password;
-    const q = 'SELECT id FROM test.users WHERE email=? and password=?';
-    db.query(q, [userEmail, userPassword], (err, data) => {
-        if (err) {
-            return res.json(err);
-        }
-        return data.length > 0 ? res.json(true) : res.json(false);
-    });
-});
-
-app.delete('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const q = 'DELETE FROM users WHERE id = ?';
-    db.query(q, [userId], (err, data) => {
-        if (err) {
-            return res.json(err);
-        }
-        return res.json('user has been deleted');
-    });
-});
+app.delete('/users', checkAuth, deleteUsers);
 
 app.listen(8800, () => {
-    console.log('hi there lol ');
+    console.log('server is working properly');
 });
